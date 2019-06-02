d4761f8805a5361586d9ba210bbf96e3a523998a
ayushig2000 <49480958+ayushig2000@users.noreply.github.com>
2019-06-02 16:25:57 +0530

    Add files via upload

---
+++ ex32.py
@@ -0,0 +1,22 @@
+the_count = [1,2,3,4]
+fruit = ['apple', 'banana','mango']
+change = [1,'ayushi',2,4]
+
+for number in count:
+    print(f"this is count {number}")
+
+for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+for i in change:
+    print(f"I got {i}")
+
+elements = []
+
+for i in range(0, 6):
+print(f"Adding {i} to the list.")
+
+elements.append(i)
+
+for i in elements:
+print(f"Element was: {i}")
